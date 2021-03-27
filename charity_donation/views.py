686f56f87c46b453151840240702adb1762b06a2
Rafal765 <r_kozak@tlen.pl>
2021-03-27 18:57:24 +0100

    Task #3 - new user form

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,12 +1,14 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
 from charity_donation.models import Donation, Institution, Category
+from charity_donation.forms import SignUpForm
 
 
 class LandingPage(View):
     def get(self, request):
         bag_number = Donation.objects.all().count()
-        institution_number = Institution.objects.all().count()
+        institution_number = Donation.objects.values('institution__name').distinct().count()
         institution_foundation = Institution.objects.filter(type='f').order_by('name')
         institution_organization = Institution.objects.filter(type='o').order_by('name')
         institution_local = Institution.objects.filter(type='z').order_by('name')
@@ -32,4 +34,13 @@ class Login(View):
 
 class Register(View):
     def get(self, request):
-        return render(request, 'charity_donation/register.html')
+        form = SignUpForm()
+        return render(request, 'charity_donation/register.html', {'form': form})
+
+    def post(self, request):
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('../login#login')
+        return render(request, 'charity_donation/register.html', {'form': form})
+
