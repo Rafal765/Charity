22f1ee22f87366ef0647d8857abeadee958da344
Rafal765 <r_kozak@tlen.pl>
2021-03-29 20:37:38 +0200

    Adjustment to register form

--- charity_donation/forms.py
+++ charity_donation/forms.py
@@ -4,11 +4,13 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=32)
+    last_name = forms.CharField(max_length=32)
     username = forms.EmailField()
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2']
+        fields = ['first_name', 'last_name', 'username', 'password1', 'password2']
 
 
 class LoginForm(forms.Form):
