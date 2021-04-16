2729447dc2fb0c550a5adfbfb7f716ec76fec662
Rafal765 <r_kozak@tlen.pl>
2021-04-16 20:31:08 +0200

    Settings adjustment

--- my_project/settings.py
+++ my_project/settings.py
@@ -20,14 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+vl2q%_9!9#of79i110g0ey^&&clv)ry184r!5b9t$r3l!ml_x'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+try:
+    from .local_settings import SECRET_KEY, DEBUG, ALLOWED_HOSTS
+except ImportError:
+    SECRET_KEY = "placeholder"
+    DEBUG = True
+    ALLOWED_HOSTS = []
 
 # Application definition
 
